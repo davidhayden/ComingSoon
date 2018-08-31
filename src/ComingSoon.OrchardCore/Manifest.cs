9ccae8dd72543ac03f301baf6f584e7b529c69f0
davidhayden <davidhayden@users.noreply.github.com>
2018-08-31 15:31:08 -0400

    Add initial files

---
+++ src/ComingSoon.OrchardCore/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Coming Soon",
+    Author = "David Hayden",
+    Website = "http://www.davidhayden.me",
+    Version = "2.0.0",
+    Description = "Bootstrap 4 theme with an under construction landing page based on Start Bootstrap template."
+)]
